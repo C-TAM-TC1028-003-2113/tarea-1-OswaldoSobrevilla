b500a1adda297dabd8e78aebf8839818efdc5531
OswaldoSobrevilla <89714906+OswaldoSobrevilla@users.noreply.github.com>
2021-08-29 21:49:36 -0500

    Termine el ejericio 5

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    palabras = int(input("Dame el número de palabras: "))
+    paginas = 1
+    if palabras < 475:
+        paginas = 1
+        costo = (paginas*60)*0.9
+        print("El costo de la publicación es:", costo)
+    else:
+             palabras > 475
+             paginas = 2
+             costo = (paginas*60)*0.9
+             print("El costo de la publicación es:", costo)
 
 
 if __name__ == '__main__':
