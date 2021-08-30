1fea3b49ebeb62f2bda98e0789e20f119cf7b691
OswaldoSobrevilla <89714906+OswaldoSobrevilla@users.noreply.github.com>
2021-08-29 22:45:31 -0500

    Termine el ejercicio 6

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    gramos = float(input("Dame la harina en gramos: "))
+    levadura = gramos * 0.05
+    print("Necesitas estos gramos de levadura", levadura)
 
 
 if __name__ == '__main__':
